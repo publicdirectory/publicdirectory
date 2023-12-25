3051c1e47c08354b328882e1403240343964680a
Philipp Tsipman <philtsip@gmail.com>
2023-12-25 01:17:40 -0500

    fix null test

--- app/api/threads/route.ts
+++ app/api/threads/route.ts
@@ -13,11 +13,17 @@ export async function POST(req: NextRequest) {
 
     const html = response.data
     const dom = new JSDOM(html)
-    const title = dom.window.document.querySelector("title").textContent
+    const titleElement = dom?.window.document.querySelector("title")
+    const title = titleElement ? titleElement.textContent : ""
 
-    if (title.toLowerCase().includes(threads_handle.toLowerCase())) {
+    if (
+      dom &&
+      title &&
+      title.toLowerCase().includes(threads_handle.toLowerCase())
+    ) {
       exists = true
     } else {
+      // Handle case when dom is null or title is empty or does not contain threads_handle
     }
 
     return NextResponse.json({ threads_exists: exists })
