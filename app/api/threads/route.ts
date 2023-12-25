f6c915207cebb012bee484cd6594be44a4875a27
Philipp Tsipman <philtsip@gmail.com>
2023-12-25 09:36:46 -0500

    remove jsdom - don't need it

--- app/api/threads/route.ts
+++ app/api/threads/route.ts
@@ -1,5 +1,4 @@
 import { NextRequest, NextResponse } from "next/server"
-import { JSDOM } from "jsdom"
 import axios from "axios"
 
 export async function POST(req: NextRequest) {
@@ -12,18 +11,9 @@ export async function POST(req: NextRequest) {
     let exists = false
 
     const html = response.data
-    const dom = new JSDOM(html)
-    const titleElement = dom?.window.document.querySelector("title")
-    const title = titleElement ? titleElement.textContent : ""
 
-    if (
-      dom &&
-      title &&
-      title.toLowerCase().includes(threads_handle.toLowerCase())
-    ) {
+    if (html.includes(threads_handle.toLowerCase())) {
       exists = true
-    } else {
-      // Handle case when dom is null or title is empty or does not contain threads_handle
     }
 
     return NextResponse.json({ threads_exists: exists })
