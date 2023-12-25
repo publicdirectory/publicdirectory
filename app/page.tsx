0dd80df0c784c4cfbbb187fd280c1578bbedc47b
Philipp Tsipman <philtsip@gmail.com>
2023-12-25 09:32:06 -0500

    added actions init

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,21 @@ import {
   TableBody,
   Table,
 } from "@/components/ui/table"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogFooter,
+  DialogClose,
+} from "@/components/ui/dialog"
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Pencil, Plus } from "lucide-react"
+import directory from "../public/directory.json"
 
 export default function Home() {
   const [inputText, setInputText] = useState(
@@ -41,7 +55,7 @@ export default function Home() {
     return handles.map((handle) => {
       return {
         x_handle: handle.slice(1),
-        threads_handle: handle.slice(1),
+        threads_handle: "",
         other: "",
       }
     })
@@ -71,10 +85,20 @@ export default function Home() {
 
     const updatedHandles = await Promise.all(
       handles.map(async (handle) => {
-        const threadsExists = await checkThreadsHandle(handle.threads_handle)
-        return {
-          ...handle,
-          threads_exists: threadsExists,
+        if (handle.x_handle in directory) {
+          return {
+            ...handle,
+            threads_exists: true,
+            // @ts-ignore
+            threads_handle: directory[handle.x_handle].threads_handle,
+          }
+        } else {
+          const threadsExists = await checkThreadsHandle(handle.x_handle)
+          return {
+            ...handle,
+            threads_exists: threadsExists,
+            threads_handle: threadsExists ? handle.x_handle : "",
+          }
         }
       })
     )
@@ -93,7 +117,6 @@ export default function Home() {
         (You don&apos;t have to format the handles in any way, just paste them
         in)
       </p>
-
       <textarea
         className="w-full max-w-lg p-4 mb-4 border rounded-md h-auto"
         placeholder="Paste in X(Twitter) handles here"
@@ -109,7 +132,6 @@ export default function Home() {
       >
         Find
       </Button>
-
       <Table>
         <TableHeader>
           <TableRow>
@@ -117,6 +139,7 @@ export default function Home() {
             <TableHead className="text-slate-900">X(Twitter) Handle</TableHead>
             <TableHead className="text-slate-900">Threads Handle</TableHead>
             <TableHead className="text-slate-900">Follow on Threads</TableHead>
+            <TableHead className="text-slate-900">Actions</TableHead>
             {/* <TableHead className="text-slate-900">Other</TableHead> */}
           </TableRow>
         </TableHeader>
@@ -162,11 +185,91 @@ export default function Home() {
                   ""
                 )}
               </TableCell>
+              <TableCell>
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Button variant="outline" className="font-normal">
+                      {handle.threads_exists ? (
+                        <>
+                          <Pencil size={16} className="mr-2" />
+                          <span>Edit</span>
+                        </>
+                      ) : (
+                        <>
+                          <Plus size={16} className="mr-2" />
+                          <span>Add</span>
+                        </>
+                      )}
+                      &nbsp;Theads
+                    </Button>
+                  </DialogTrigger>
+                  <DialogContent className="sm:max-w-[500px]">
+                    <DialogHeader>
+                      <DialogTitle>
+                        {handle.threads_exists ? "Edit" : "Add"} Threads handle
+                      </DialogTitle>
+                      {/* <DialogDescription>
+                        Make changes to your profile here. Click save when
+                        you're done.
+                      </DialogDescription> */}
+                    </DialogHeader>
+                    <div className="grid gap-4 py-4">
+                      <div className="grid grid-cols-6 items-center gap-4">
+                        <Label htmlFor="name" className="text-right col-span-2">
+                          X(Twitter) Handle
+                        </Label>
+                        <p className="col-span-4 text-sm ml-3">
+                          {handle.x_handle}
+                        </p>
+                      </div>
+                      <div className="grid grid-cols-6 items-center gap-4">
+                        <Label
+                          htmlFor="username"
+                          className="text-right col-span-2"
+                        >
+                          Threads Handle
+                        </Label>
+                        <Input
+                          id="username"
+                          value={handle.threads_handle}
+                          className="col-span-4"
+                        />
+                      </div>
+                    </div>
+                    <DialogFooter>
+                      <DialogClose asChild>
+                        <Button type="button" variant="outline">
+                          Close
+                        </Button>
+                      </DialogClose>
+                      <Button type="submit">Save changes</Button>
+                    </DialogFooter>
+                  </DialogContent>
+                </Dialog>
+              </TableCell>
               {/* <TableCell>{handle.other}</TableCell> */}
             </TableRow>
           ))}
         </TableBody>
       </Table>
+      <p className="mt-8">
+        <a
+          href="https://github.com/publicdirectory/publicdirectory"
+          target="_blank"
+          className="underline text-blue-600"
+        >
+          Github code
+        </a>
+        . Made with ❤️ by{" "}
+        <a
+          href="https://www.threads.net/@philtsip"
+          target="_blank"
+          className="underline text-blue-600"
+        >
+          philtsip
+        </a>{" "}
+        in NYC
+      </p>
     </div>
   )
 }
