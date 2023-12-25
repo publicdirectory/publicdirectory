ad6e3c488033360c97b2ef813003f64fbb3e33a9
Philipp Tsipman <philtsip@gmail.com>
2023-12-25 12:28:22 -0500

    added Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,24 @@
-import type {Metadata} from 'next'
-import {Inter} from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
+import Script from "next/script"
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-	title: 'Public Directory',
-	description: 'A delightful, public, open-source social media directory',
+  title: "Public Directory",
+  description: "A delightful, public, open-source social media directory",
 }
 
-export default function RootLayout({children}: {children: React.ReactNode}) {
-	return (
-		<html lang="en">
-			<body className={inter.className}>{children}</body>
-		</html>
-	)
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-7WYWX5KBTZ" />
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
 }
